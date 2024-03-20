8811c5953d1ad57706316336ddfb9eccc71d4226
Quyet <huuquyetng@gmail.com>
2024-03-20 23:28:32 +0700

    feat: add cards demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ import { Canvas } from '@react-three/fiber'
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
-const Carousel = dynamic(() => import('@/three/Carousel').then((mod) => mod.Carousel), {})
+const CardsDemo = dynamic(() => import('@/three/CardsDemo').then((mod) => mod.CardsDemo), {})
 
 export default function Page() {
   return (
     <>
-      <div className="mx-auto flex w-full flex-col flex-wrap items-center md:flex-row lg:w-4/5">
+      <div>
         <Canvas>
           <Suspense fallback={null}>
-            <Carousel />
+            <CardsDemo />
           </Suspense>
         </Canvas>
       </div>
